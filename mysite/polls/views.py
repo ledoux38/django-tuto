a011ce77d587026ea845cf287e7396b9288c77d5
Florian LEDOUX <florian.ledoux@synergee.com>
2020-12-10 15:37:44 +0100

    update

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,18 +1,25 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
 from django.template import loader
 
 from .models import Question
 
 
-def index(request: object) -> HttpResponse:
+def index(request: object) -> render:
     lastest_question_list: list = Question.objects.order_by('-pub_date')[:5]
     template: object = loader.get_template('polls/index.html')
     context: dict = {'lastest_question_list': lastest_question_list}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 
-def detail(question_id) -> HttpResponse:
-    return HttpResponse("You're looking at question %s." % question_id)
+def detail(request: object, question_id: int) -> render:
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question des not exist")
+    context: dict = {'question': question}
+    print(context)
+    return render(request, 'polls/detail.html', context)
 
 
 def result(question_id) -> HttpResponse:
