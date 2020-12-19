31c15bfb1ad98a2676d79a1e30398abd306da20b
Ledoux38 <ledoux.florian30@gmail.com>
2020-12-19 14:27:51 +0100

    add generic views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,27 +2,28 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
 
-def index(request: object) -> render:
-    lastest_question_list: list = Question.objects.order_by('-pub_date')[:5]
-    template: object = loader.get_template('polls/index.html')
-    context: dict = {'lastest_question_list': lastest_question_list}
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'lastest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('pub_date')[:5]
 
 
-def detail(request: object, question_id: int) -> render:
-    question = get_object_or_404(Question, pk=question_id)
-    context: dict = {'question': question}
-    return render(request, 'polls/detail.html', context)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-def results(request: object, question_id: int) -> render:
-    question = get_object_or_404(Question, pk=question_id)
-    context: dict = {'question': question}
-    return render(request, 'polls/results.html', context)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request: object, question_id: int) -> HttpResponseRedirect:
